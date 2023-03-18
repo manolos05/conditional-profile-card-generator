7158d6dfa177379a66c5df41cfae062831b0cab1
Manuel Salazar <manusalh@gmail.com>
2023-03-18 14:34:36 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,71 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = variables.name;
+  if (variables.name === null) {
+    variables.name = "Your Name";
+    name = variables.name;
+  }
+
+  let lastname = variables.lastname;
+  if (variables.lastname === null) {
+    variables.lastname = "&  Last Name";
+    lastname = variables.lastname;
+  }
+
+  let role = variables.role;
+  if (variables.role === null) {
+    variables.role = "Role";
+    role = variables.role;
+  }
+
+  let ciudad = variables.city;
+  if (variables.city === null) {
+    variables.city = "City";
+    ciudad = variables.city;
+  }
+  let pais = variables.country;
+  if (variables.country === null) {
+    variables.country = "Country";
+    pais = variables.country;
+  }
+
+  let twitter = variables.twitter;
+  if (variables.twitter === null) {
+    variables.twitter = "";
+    twitter = variables.twitter;
+  }
+
+  let github = variables.twitter;
+  if (variables.github === null) {
+    variables.github = "";
+    github = variables.github;
+  }
+
+  let linkedin = variables.linkedin;
+  if (variables.linkedin === null) {
+    variables.linkedin = "";
+    linkedin = variables.linkedin;
+  }
+
+  let instagram = variables.instagram;
+  if (variables.instagram === null) {
+    variables.instagram = "";
+    instagram = variables.instagram;
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1 >${variables.name} ${variables.lastname}</h1>
+          <h2>"${variables.role}"</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}" target="_blank"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}" target="_blank"><i class="bi bi-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}" target="_blank"><i class="bi bi-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}" target="_blank"><i class="bi bi-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -61,7 +114,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
